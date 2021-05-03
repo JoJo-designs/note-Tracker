51db0c1f8e206f2368868ff8110cfef92e33f770
jojo <jodiladouceur.design@gmail.com>
2021-05-03 11:52:35 -0400

    edits to files

--- public/path.js
+++ public/path.js
@@ -7,11 +7,20 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const notes = []
+
 // Route for landing page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 // Route for notes page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+// Route to add a new note to the page
+app.post('/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.push(notes)
+    console.log("New note has been added.")
+})
 
 // start program with node path.js in write file 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
