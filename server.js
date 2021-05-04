2341e1588d51920e4cbbaad813f6e6af23e0ccb4
jojo <jodiladouceur.design@gmail.com>
2021-05-04 11:16:53 -0400

    edits to files move and changed file names

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const notesList = require('../../db/db.json')
+const noteList = 'db/db.json'
 
 
 // Route for landing page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 // Route for notes page
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(notesList));
 
 // Route to add a new note to the page
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     console.log("New note has been added.")
 })
