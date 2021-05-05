d0a08a13068871eed177eed6ae59726349062e02
jojo <jodiladouceur.design@gmail.com>
2021-05-05 10:54:29 -0400

    edits but not really

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// const notes = [
-//     {
-//         title:'Test Title',
-//         text:'Test text'
-//     },
-// ];
-// module.exports = notes;
+const notes = [
+    {
+        title:'Test Title',
+        text:'Test text'
+    },
+];
+module.exports = notes;
 
 // Route for landing page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
@@ -22,8 +22,9 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 // Route for notes page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-// app.get('/api/notes', (req, res) => res.json(notes));
-app.get('/api/notes', (req, res) => res.json("db/db.json"));
+ app.get('/api/notes', (req, res) => res.json(notes));
+// I did this but I doesn't work 
+//app.get('/api/notes', (req, res) => res.json("db/db.json"));
 
 // Route to add a new note to the page
 app.post('/api/notes', (req, res) => {
