90b93ba166ea2cbda745b07088c082fe43e14da7
jojo <jodiladouceur.design@gmail.com>
2021-05-06 11:12:37 -0400

    edits to files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs')
+const autoincrement = require('autoincrement');
 
 const app = express();
 const PORT = 3000;
@@ -10,22 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// const notes = [
-//     {
-//         title:'Test Title',
-//         text:'Test text'
-//     },
-// ];
-// module.exports = notes;
-
 // Route for landing page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 // Route for notes page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-//Top one works with array on this page
- //app.get('/api/notes', (req, res) => res.json(notes));
 
 // Route to hopefully read the data.
 app.get('/api/notes', (req, res) => {
@@ -36,9 +27,11 @@ app.get('/api/notes', (req, res) => {
 // Route to put save data into the read file
 app.post('/api/notes', (req, res) => {
     const notes = req.body
-    dbArray.push(notes)
-    console.log(notes)
-    console.log(dbArray)
+    const id = autoincrement
+    id == (autoincrement + 1);
+        dbArray.push(notes)
+        console.log(notes) 
+        console.log(dbArray)
 }); 
 
 
